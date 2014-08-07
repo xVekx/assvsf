b576dad499b99a283f1ec31bda29ce65798e5f64
xVekx <xxxvekxxx@gmail.com>
2014-08-07 18:18:09 +0400

    v002

--- assformat.cpp
+++ assformat.cpp
@@ -13,13 +13,11 @@ bool AssFormat::ReadFile(QString File)
 	{
 		return false;
 	}
-
 	QTextStream TextStream(&AssFile);
 	while (!TextStream.atEnd())
 	{
 		AssText << TextStream.readLine();
 	}
-
 	AssFile.close();
 	return true;
 }
@@ -32,14 +30,11 @@ bool AssFormat::WriteFile(QString File)
 	{
 		return false;
 	}
-
 	QTextStream TextStream(&AssFile);
-
 	for(int i=0; i<AssText.size(); i++)
 	{
 		TextStream << AssText.at(i)<<endl;
 	}
-
 	AssFile.close();
 	return true;
 }
@@ -54,57 +49,39 @@ void AssFormat::NumerText()
 	{
 		if(AssText.at(i).indexOf(QRegExp("^\\[")) > -1)
 		{
-			qDebug()<<AssText.at(i);
 			SectionEvents = false;
 		}
-
 		//Ищем секцию Events
 		if(AssText.at(i).indexOf(QRegExp("^\\[Events\\]")) > -1)
 		{
-			qDebug()<<AssText.at(i);
 			SectionEvents = true;
 		}
-
 		if(SectionEvents)
 		{
 			//Находим количество запятых
-
 			if(AssText.at(i).indexOf(QRegExp("^Format:")) > -1)
 			{
 				int temp = 0;
 				for(Virgule=0;Virgule<20;Virgule++)
 				{
 					temp = AssText.at(i).indexOf(",",temp+1);
-					//qDebug()<<temp;
 					if( temp == -1)
 					{
 						break;
 					}
 				}
-				qDebug()<<Virgule;
+				qDebug()<<"Virgule"<<Virgule;
 			}
 			if(AssText.at(i).indexOf(QRegExp("^Dialogue:")) > -1)
 			{
-				//qDebug()<<AssText.at(i);
 				int Offset = 0;
 				for(int v=0;v<Virgule;v++)
 				{
 					Offset = AssText.at(i).indexOf(",",Offset+1);
-					//qDebug()<<temp;
 				}
-				//qDebug()<<Offset;
-
-				//qDebug()<<AssText.at(i).mid(0,Offset+1);
-
 				QString TextNumStr = AssText.at(i).mid(0,Offset+1) + QString("%1").arg(NumStr);
-
-				qDebug()<<TextNumStr;
-
 				NumStr++;
-
 				AssText.replace(i,TextNumStr);
-
-			//qDebug()<<AssText.at(i).indexOf(QRegExp(","));
 			}
 		}
 	}
--- main.cpp
+++ main.cpp
@@ -9,21 +9,19 @@ int main(int argc, char *argv[])
 	{
 		qDebug()<<"Command format: assvsf in.ass out.ass";
 	}
-
 	QString InFile(argv[1]);
 	QString OutFile(argv[2]);
-
 	qDebug()<<InFile<<"->"<<OutFile;
-
 	if(!AF.ReadFile(InFile))
 	{
 		qDebug()<<"Not Open In File";
-		return 0;
+		return -1;
 	}
-
 	AF.NumerText();
-
-	AF.WriteFile(OutFile);
-
+	if(!AF.WriteFile(OutFile))
+	{
+		qDebug()<<"Not Save Out File";
+		return -1;
+	}
 	return 0;
 }
